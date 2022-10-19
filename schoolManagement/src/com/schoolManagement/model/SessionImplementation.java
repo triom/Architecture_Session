1afd04fe41c14eb776c304600f18e33cd5e4d5a2
SlimKhiari <slim.khiari@ens.uvsq.fr>
2022-10-19 19:39:10 +0200

    interface + implementation (non fini)

---
+++ schoolManagement/src/com/schoolManagement/model/SessionImplementation.java
@@ -0,0 +1,29 @@
+package com.schoolManagement.model;
+
+public class SessionImplementation implements SessionInterface{
+
+	@Override
+	public String createEU() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteEU() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUE() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String listEU() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ schoolManagement/src/com/schoolManagement/model/SessionInterface.java
@@ -0,0 +1,8 @@
+package com.schoolManagement.model;
+
+public interface SessionInterface {
+	public String createEU();
+	public String deleteEU();
+	public String getUE();
+	public String listEU();
+}
