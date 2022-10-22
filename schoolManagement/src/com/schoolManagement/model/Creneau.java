c275ebdf80fe65f24da8822a6c1682984c50d646
nessrine80 <49169444+nessrine80@users.noreply.github.com>
2022-10-22 15:57:02 +0200

    création de classe créneau

--- schoolManagement/src/com/schoolManagement/model/Creneau.java
+++ schoolManagement/src/com/schoolManagement/model/Creneau.java
@@ -1,5 +1,51 @@
 package com.schoolManagement.model;
 
 public class Creneau {
+   private  String idCreneau;
+   private  String debut;
+   private  String fin;
+   private  String jour;
 
+    public Creneau(String idCreneau, String debut, String fin, String jour) {
+        this.idCreneau = idCreneau;
+        this.debut = debut;
+        this.fin = fin;
+        this.jour = jour;
+    }
+
+    public String getIdCreneau() {
+        return idCreneau;
+    }
+
+    public void setIdCreneau(String idCreneau) {
+        this.idCreneau = idCreneau;
+    }
+
+    public String getDebut() {
+        return debut;
+    }
+
+    public void setDebut(String debut) {
+        this.debut = debut;
+    }
+
+    public String getFin() {
+        return fin;
+    }
+
+    public void setFin(String fin) {
+        this.fin = fin;
+    }
+
+    public String getJour() {
+        return jour;
+    }
+
+    public void setJour(String jour) {
+        this.jour = jour;
+    }
+   
+   
+    
+    
 }
